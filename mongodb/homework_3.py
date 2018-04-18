3da61c9f215c685d224f2db1658568dc80fbee0e
nyurdakul <ny2266@columbia.edu>
2018-04-17 23:49:36 -0400

    everything works but part B

--- mongodb/homework_3.py
+++ mongodb/homework_3.py
@@ -7,14 +7,16 @@ database = client.test
 
 collection = database.movies
 
-#collection.update_many( {"genres:" : "Drama", "rated" : "NOT RATED"}, {"$set": {"rated" : "Pending Rating"}} )
+collection.update_many(
+	{"genres:" : "Drama", "rated" : "NOT RATED"},
+	{"$set": {"rated" : "Pending rating"}})
 
-#collection.insert({"title" : "Call me By Your Name",
-#		   "year" : 2017,
-#		   "countries" : ["Italy", "France", "Brazil", "USA"],
-#		   "genres" : ["Drama", "Romance"],
-#		   "directors" : ["Luca Guadagnino"],
-#		   "imdb" : {"id" : 5726616, "rating" : 8.0, "votes": 98821}})
+collection.insert({"title" : "Call me By Your Name",
+		   "year" : 2017,
+		   "countries" : ["Italy", "France", "Brazil", "USA"],
+		   "genres" : ["Drama", "Romance"],
+		   "directors" : ["Luca Guadagnino"],
+		   "imdb" : {"id" : 5726616, "rating" : 8.0, "votes": 98821}})
 
 dramaCount = collection.aggregate( [
   { "$match": { "genres":  "Drama"} },
@@ -23,9 +25,34 @@ dramaCount = collection.aggregate( [
 
 pprint.pprint(list(dramaCount))
 
+
 turkeyCount = collection.aggregate( [
-  { "$match": { "countries":  "Turkey", "rated" : "NOT RATED"} },
-  { "$group": { "_id": {"country" : "Turkey", "rating" : "NOT RATED"}, "count": { "$sum": 1 } } }
+  { "$match": { "countries":  "Turkey" , "rated" : "Pending rating"}},
+  { "$group": { "_id": {"country" : "Turkey", "rated" : "Pending rating"}, "count": { "$sum": 1 } } }
 ] )
 
 pprint.pprint(list(turkeyCount))
+
+names = database.names
+ages = database.ages
+
+names.insert_many([
+	{"firstname" : "Nazli", "lastname": "Yurdakul"},
+	{"firstname" : "Can", "lastname" : "Akdere"},
+	{"firstname" : "Montse", "lastname" : "Pladevall"}
+])
+
+ages.insert_many([
+	{"name" : "Nazli", "age" : 21},
+	{"name" : "Can", "age" : 20},
+	{"name" : "Montse", "age" : 19}
+])
+
+lookupResult = names.aggregate([
+	{"$lookup": {"from" : "ages", "localField" : "firstname", "foreignField" : "name", "as" : "records"}}
+])
+
+pprint.pprint(list(lookupResult))
+
+
+
